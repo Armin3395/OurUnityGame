119767caf07d5ee2aa596f81f10847d04e676f8c
Armin <arminby1000@gmail.com>
2023-01-24 21:47:51 +0330

    Removed Extra Codes (idk why they existed)
    they made errors

--- Assets/Codes/SwordScript.cs
+++ Assets/Codes/SwordScript.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.SocialPlatforms;
 
 public class SwordScript : MonoBehaviour
 {
